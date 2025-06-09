c39745d2885de5933bcaa23803d968f283ed27a5
Alexander Höllinger <a.hoellinger@ewemed.at>
2025-06-09 19:47:31 +0200

    com 5

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,13 @@ const dev = process.argv.includes('dev');
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+            assets: 'build',
+            fallback: undefined,
+            precompress: false,
+            strict: true
+		})
 	},
 	paths: {
 		base: dev ? '' : process.env.BASE_PATH,
